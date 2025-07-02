c64da244a1177f3dbc2c404939d1993cc7054d1e
Arunima Gururani <ArunimaGu@emids.com>
2025-07-02 18:08:57 +0530

    changes to extent report

--- src/main/java/org/practice/utils/ExtentReporter.java
+++ src/main/java/org/practice/utils/ExtentReporter.java
@@ -3,12 +3,20 @@ package org.practice.utils;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
+import java.io.File;
+
 public class ExtentReporter {
 
 
     public  static ExtentReports generateReport(){
 
-        String filePath= System.getProperty("user.dir")+"\\reports\\index.html";
+        String reportDirPath = System.getProperty("user.dir") + "/reports";
+        File reportDir = new File(reportDirPath);
+        if (!reportDir.exists()) {
+            reportDir.mkdirs();
+        }
+
+        String filePath= reportDirPath+"/index.html";
         ExtentSparkReporter reports = new ExtentSparkReporter(filePath);
         reports.config().setReportName("Complete Automation Report");
         reports.config().setDocumentTitle("Test Results for Application");
